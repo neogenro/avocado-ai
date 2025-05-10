3840fcabc3ed5043adcf8bdfeb20b5e1f5edc569
Geancarlo_Cojocaru <t4t_geancarlo@yahoo.com>
2025-05-10 13:00:49 +0300

    homepage

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
           <div className="flex items-center gap-4">
             <ThemeToggle />
             <Button asChild>
-              <Link href="#get-started">Începe Acum</Link>
+              <Link href="/login">Începe Acum</Link>
             </Button>
           </div>
         </div>
